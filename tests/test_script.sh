54d3ca34466b143264da02dcee1a042b0d1f0326
Joseph Hale <joseph.hale@rapp.com>
2022-11-21 18:48:56 -0600

    first test

--- tests/test_script.sh
+++ tests/test_script.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
+function assertResponse() {
+    local testName="${1}"
+    local expected="${2}"
+    local result="${3}"
+    local response="PASS"
+    if [[ ${expected} != ${result}]]
+    then
+    
+        echo "${testName}: FAILED"
+        echo "   EXPECTED: {expected}"
+        echo "   RESULT: {result}"
+    else
+         echo "${testName}: PASS"
+    fi
+
+}
 
 
+
+function git() {
+    echo """* FA-1
+  main
+"""
+}
+source ../src/script.sh
+
+
+export CODEBUILD_SOURCE_VERSION=refs/heads/main
+result=$(getBranch)
+expected="main"
+assertResponse getBranch_envVar ${expected} ${result}
